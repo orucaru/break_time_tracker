6a6dc5cff75fb222aa9e873ea7b55fc2b581ad25
orucaru <b30205k@gmail.com>
2025-03-23 00:02:32 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :breaks, only: [:index, :create, :update]
   root 'breaks#index'  # デフォルトのルートを breaks#index に設定
+  get 'index', to: 'breaks#index'
 end
 

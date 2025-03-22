72c098a5c6f0da5742bcd8ec1df435c450c217db
orucaru <b30205k@gmail.com>
2025-03-23 01:15:42 +0900

    Fix routing issue, update CI workflow, modify break_time_tracker

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :breaks, only: [:index, :create, :update]
-  root 'breaks#index'  # デフォルトのルートを breaks#index に設定
   get 'index', to: 'breaks#index'  # 'index' のルートを設定
 end
 

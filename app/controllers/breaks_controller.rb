08efc9f9527b4be8e73faa97bcabcc1564b7b8d4
orucaru <b30205k@gmail.com>
2025-03-22 16:31:19 +0900

    Fix issues to pass CI checks

--- app/controllers/breaks_controller.rb
+++ app/controllers/breaks_controller.rb
@@ -7,20 +7,18 @@ class BreaksController < ApplicationController
   def create
     @break = Break.new(start_time: Time.now)  # 休憩開始時に現在時刻を設定
     if @break.save
-      redirect_to breaks_path, notice: '休憩が開始されました！'
+      redirect_to breaks_path, notice: "休憩が開始されました！"
     else
-      redirect_to breaks_path, alert: '休憩開始に失敗しました。'
+      redirect_to breaks_path, alert: "休憩開始に失敗しました。"
     end
   end
 
   def update
     @break = Break.find(params[:id])  # 特定の休憩を取得
     if @break.update(end_time: Time.now)  # 終了時刻を現在時刻で更新
-      redirect_to breaks_path, notice: '休憩が終了しました！'
+      redirect_to breaks_path, notice: "休憩が終了しました！"
     else
-      redirect_to breaks_path, alert: '休憩終了に失敗しました。'
+      redirect_to breaks_path, alert: "休憩終了に失敗しました。"
     end
   end
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :breaks, only: [:index, :create, :update]
-  root 'breaks#index'  # デフォルトのルートを breaks#index に設定
+  resources :breaks, only: [ :index, :create, :update ]
+  root "breaks#index"  # デフォルトのルートを breaks#index に設定
 end
-
